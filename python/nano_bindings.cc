a2e1a12aa2d4ecfc50f3c639f3358a029ddb0426
Song <songyuyan2001@hotmail.com>
2025-09-24 11:48:09 -0700

    be able to process numpy array

--- python/nano_bindings.cc
+++ python/nano_bindings.cc
@@ -1,5 +1,7 @@
 #include <nanobind/nanobind.h>
-#include <nanobind/stl/string.h>
+#include <nanobind/ndarray.h>
+
+#include <iostream>
 
 #include "py_cpp_lib.h"
 
@@ -10,12 +12,20 @@ namespace
   PyCppInterop::CcLib procObj;
 };
 
+// Define a type alias for a CPU-resident RGB image using nanobind.
+// The shape `<-1, -1, 3>` means it's a 3-channel image of any height and width.
+using RGB8Image = nb::ndarray<uint8_t, nb::shape<-1, -1, 3>, nb::device::cpu>;
 
-int add(int a, int b) {
-  procObj.procRGB8ToYUV420();
-  return a + b;
+void process_image(RGB8Image data) {
+  // You can access the raw data pointer for processing
+  // nb::ndarray provides a simple interface to access metadata
+  std::cout << "Processing image with nanobind" << std::endl;
+  if (data.ndim() != 3) {
+    throw std::runtime_error("Input array must be a 3D image.");
+  }
+  std::cout << "Processing image of shape (" << data.shape(0) << ", " << data.shape(1) << ")" << std::endl;
 }
 
 NB_MODULE(nano_ext, m) {
-  m.def("add", &add, "A function which adds two numbers");
+  m.def("process_image", &process_image, "process RGB8 image");
 }
--- python/nano_main.py
+++ python/nano_main.py
@@ -1,3 +1,8 @@
 import nano_ext
+import numpy as np
 
-nano_ext.add(1, 2)
+# Create a dummy 100x100 RGB image with some data
+image = np.full((100, 100, 3), 100, dtype=np.uint8)
+print("Original image (first pixel):", image[0, 0, :])
+
+nano_ext.process_image(image)
