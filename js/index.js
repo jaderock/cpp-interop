4a9ca145a9bfb599ee7849cdbd7c6d3924fa6409
Song <songyuyan2001@hotmail.com>
2025-09-25 13:25:41 -0700

    js example without addon is added

---
+++ js/index.js
@@ -0,0 +1,4 @@
+console.log("Hello World!");
+console.log(`Current Node.js version: ${process.version}`);
+//const addon = require('./my_addon_lib.node');
+//console.log(addon.hello()); // Should output "Hello from C++ Addon!"
